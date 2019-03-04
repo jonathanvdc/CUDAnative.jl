04452787749b2ee412ddfea63804c1749edd0833
jonathanvdc <jonathan.vdc@outlook.com>
2019-03-04 15:19:41 +0100

    Define interrupt tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -63,6 +63,7 @@ if CUDAnative.configured
         else
             include("device/codegen.jl")
             include("device/execution.jl")
+            include("device/interrupts.jl")
             include("device/pointer.jl")
             include("device/array.jl")
             include("device/intrinsics.jl")
