14826e725a70232e3ec2cf0d859ccf23e5943cae
jonathanvdc <jonathan.vdc@outlook.com>
2019-03-11 18:57:46 +0100

    Add a comprehensive GC test

--- test/runtests.jl
+++ test/runtests.jl
@@ -69,6 +69,7 @@ if CUDAnative.configured
             include("device/cuda.jl")
             include("device/intrinsics.jl")
             include("device/threading.jl")
+            include("device/gc.jl")
 
             #include("examples.jl")
         end
